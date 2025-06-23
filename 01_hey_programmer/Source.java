76a5c96b35b00031a4d65171230431c0208a0b2f
BilalNaseem1 <bilalnaseem1@hotmail.com>
2025-06-23 21:45:46 +0500

    restructuring

---
+++ 01_hey_programmer/Source.java
@@ -0,0 +1,12 @@
+
+class Source {
+    
+    public static String greet(String s) {
+        return "hey "+s;
+    }
+    public static void main (String[] args) {
+    String output = greet("Bilal");
+    System.out.println(output);
+}
+}
+

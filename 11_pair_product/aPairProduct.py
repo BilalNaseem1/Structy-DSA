6fffa1d460878adc82dd5799d490f40d50292d02
BilalNaseem1 <bilalnaseem1@hotmail.com>
2025-07-02 21:22:01 +0500

    pair product

---
+++ 11_pair_product/aPairProduct.py
@@ -0,0 +1,27 @@
+# pair product
+# Write a function, pair_product, that takes in a list and a target product as arguments. The function should return a tuple containing a pair of indices whose elements multiply to the given target. The indices returned must be unique.
+
+# Be sure to return the indices, not the elements themselves.
+
+# There is guaranteed to be one such pair whose product is the target.
+
+def pairProduct(numbers, target_product):
+    hashmap = {}
+
+    for idx, value in enumerate(numbers):
+        if value not in hashmap:
+            hashmap[value] = idx
+
+        y = target_product/value
+        if y in hashmap:
+            return hashmap.get(y), idx
+        
+if __name__ == "__main__":
+    output = pairProduct([4, 7, 9, 2, 5, 1], 5) # -> (4, 5)
+
+
+    print(output)
+
+
+
+        

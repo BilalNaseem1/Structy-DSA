0c5f5942138ea34192d1aaabb618d9340a310f7b
BilalNaseem1 <bilalnaseem1@hotmail.com>
2025-08-15 22:09:38 +0500

    fizzbuzz

--- 01_Intro/04_is_prime.py
+++ 01_Intro/04_is_prime.py
@@ -1,20 +1,16 @@
 from math import sqrt
 from math import isqrt
 
-# def is_prime(n):
+def is_prime(n):
 
-#     for i in range(2, sqrt(n)):
-#         if n%i == 0:
-#             return False
+    if n ==1:
+        return False
+
+    for i in range(2, isqrt(n)+ 1):
+        if n%i == 0:
+            return False
         
-#     return True
-
-
-# print(is_prime(2))
-
-# print(is_prime(3))
-
-# print(is_prime(4))
+    return True
 
 
 print(isqrt(16))
